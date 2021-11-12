fd99b1307a68a709b1f9667297a0b657ba101133
kouaouci <karimakouaouci2803@gmail.com>
2021-11-12 11:16:36 +0100

    page contact terminé

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
-
+import {User} from 'src/shared/model/user.model';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {}
+export class ContactComponent {
+  user: User = new User();
+
+  onSubmit() {
+    console.log(this.user);
+  }
+  onClick() {
+    alert('votre message bien reçu');
+  }
+}
